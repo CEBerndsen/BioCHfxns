3e1c78d154973b16f7093f071b3df204fab8dccb
Christopher Berndsen <berndsce@jmu.edu>
2018-02-26 16:37:53 -0500

    updated sat_curve.R to remove guides feature

--- sat_curve.R
+++ sat_curve.R
@@ -2,9 +2,9 @@
 library(tidyverse)
 library(broom)
 
-MM_simplefit <- function(df, guides = TRUE, plot = TRUE){
+  MM_simplefit <- function(df, plot = TRUE){
   #df should be a two column data frame with columns names 'rate' and 'conc'
-  
+  df <- df
   #Establish some values for fitting
   Kstart <- max(df$conc)/2
   Vstart <- max(df$rate)
@@ -13,32 +13,24 @@ MM_simplefit <- function(df, guides = TRUE, plot = TRUE){
   fit <- nls(rate ~ (V * conc)/(K + conc), data = df, start = list(K = Kstart, V = Vstart))
   
   #Tidy the table
-  fitstats <<- tidy(fit)
+  fitstats <- tidy(fit)
   
   #make the fit data
   mmfitdata <- data.frame(conc = seq(0, max(df$conc)*1.2, length.out = 200))
-  mmfitdata$Rate <- predict(fit, newdata = mmfitdata)
+  mmfitdata$rate <- predict(fit, newdata = mmfitdata)
   
   if(plot == TRUE){
   #make a plot
-    if(guides == TRUE) {
-    ggplot() +
-      geom_point(data = df, aes(x = conc, y = rate)) + 
-      geom_line(data = mmfitdata, aes(x = Conc, y = Rate*1000), colour = "black") +
-      geom_segment(aes(x = 0, y = (0.7021/2), xend = 210, yend = (0.7021/2)), color = "red") +
-      geom_segment(aes(x = 210, y = 0, xend = 210, yend = (0.7021/2)), color = "red") +
-      geom_hline(yintercept = 0.7021, color = "purple") +
-      annotate("text", x = 2500, y = 0.8, label = "~V[max]", color = "purple", parse = TRUE, size = 8) +
-      annotate("text", x = 600, y = 0.1, label = "~K[M]", color = "red", parse = TRUE, size = 8) +
-      geom_segment(aes(x = 450, y = 0.07, xend = 250, yend = 0.01), color = "red", arrow = arrow(length = unit(0.1, "inches")))
-  } else
-  {
+    
       ggplot() +
         geom_point(data = df, aes(x = conc, y = rate), color = "red") + 
-        geom_line(data = mmfitdata, aes(x = Conc, y = Rate*1000), colour = "black")
-    }
-  } else
+        geom_line(data = mmfitdata, aes(x = conc, y = rate), colour = "black")
+    } else
   {
     return(fitstats)
     }
 }
+
+
+df <- data_frame(conc = seq(0, 100, by = 10),
+                 rate = c(0, 3, 6, 9, 12, 14, 15, 16, 17, 17.5, 18))
